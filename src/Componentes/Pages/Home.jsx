39c48dbc99d9851976dfa83c5fdd01b6f7d87a6f
Jean Brugal <jpbrugal@hotmail.com>
2023-10-05 14:20:49 -0400

    left side done y fotos test

--- src/Componentes/Pages/Home.jsx
+++ src/Componentes/Pages/Home.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import LeftSide from '../LeftSideBar/LeftSide'
-
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import LeftSide from "../LeftSideBar/LeftSide";
+import RightSide from "../RightSideBar/RightSide";
 const Home = () => {
   return (
-    <div className='w-full'>
-      <div className='fixed top-0 z-10 w-full bg-white'>
+    <div className="w-full">
+      <div className="fixed top-0 z-10 w-full bg-white">
         <Navbar></Navbar>
       </div>
-        <div className=' flex bg-gray-100'>
-          <div className='flex-auto w-[20%] fixed top-12'>
-            <LeftSide></LeftSide>
-          </div>
+      <div className=" flex bg-gray-100">
+        <div className="flex-auto w-[20%] fixed top-12">
+          <LeftSide></LeftSide>
         </div>
+        <div className="flex-auto w-[20%] fixed right-0 top-12">
+          <RightSide></RightSide>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
